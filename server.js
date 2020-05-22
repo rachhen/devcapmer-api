7827a5786123b9d43ed94847e6b15c6fd52162d2
Rachhen <rachhen.it@outlook.com>
2020-05-23 00:01:19 +0700

    Setup mongodb connection and added log colors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Route file
 const bootcamps = require("./routes/bootcamps");
@@ -7,13 +10,30 @@ const bootcamps = require("./routes/bootcamps");
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to datanse
+connectDB();
+
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit
+  server.close(() => process.exit(1));
+});
